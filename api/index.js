21c94fcbbef095bf436907467b6033b482e1e188
yooha1003 <qtwing@naver.com>
2025-05-31 10:18:29 +0900

    modified js commit

--- api/index.js
+++ api/index.js
@@ -1,31 +1,29 @@
-const express = require("express");
-const request = require("request");
-const app = express();
+var express = require("express");
+var app = express();
 
-const baseApiUrl = "http://apis.data.go.kr/1360000/VilageFcstInfoService_2.0";
+app.get("/weather", function (req, res) {
+  const { serviceKey, numOfRows, pageNo, base_date, base_time, nx, ny } = req.query;
 
-const forwardRequest = (endpoint, req, res) => {
-    const { serviceKey, ...query } = req.query;
-    const options = {
-        url: `${baseApiUrl}/${endpoint}`,
-        qs: { serviceKey, ...query },
-    };
-    request.get(options, (error, response, body) => {
-        if (!error && response.statusCode === 200) {
-            res.setHeader("Content-Type", "application/xml;charset=utf-8");
-            res.send(body);
-        } else {
-            res.status(response?.statusCode || 500).send(error || "Internal Error");
-            console.error("API error:", response?.statusCode, error);
-        }
-    });
-};
+  var api_url = "http://apis.data.go.kr/1360000/VilageFcstInfoService_2.0/getUltraSrtNcst?";
+  var request = require("request");
+  var options = {
+    url: api_url,
+    qs: { serviceKey, numOfRows, pageNo, base_date, base_time, nx, ny },
+  };
 
-app.get("/ultra-srt-ncst", (req, res) => forwardRequest("getUltraSrtNcst", req, res));
-app.get("/ultra-srt-fcst", (req, res) => forwardRequest("getUltraSrtFcst", req, res));
-app.get("/vilage-fcst", (req, res) => forwardRequest("getVilageFcst", req, res));
-app.get("/fcst-version", (req, res) => forwardRequest("getFcstVersion", req, res));
+  request.get(options, function (error, response, body) {
+    if (!error && response.statusCode == 200) {
+      res.writeHead(200, { "Content-Type": "application/xml;charset=utf-8" });
+      res.end(body);
+    } else {
+      res.status(response.statusCode).end();
+      console.log("error = " + response.statusCode);
+    }
+  });
+});
 
-app.listen(3000, () => {
-    console.log("서버 실행 중: http://localhost:3000/ultra-srt-ncst?serviceKey=Hk%2FN%2BBOSJC66biuLuC5Xa9aZV5PLx0peXy2H7GvdsTLr3EvwE%2BkQIqnggSASFMO3TARrxwPH%2Br6qdRS0ngd0WQ%3D%3D");
+app.listen(3000, function () {
+  console.log(
+    "http://127.0.0.1:3000/weather?serviceKey=Hk%2FN%2BBOSJC66biuLuC5Xa9aZV5PLx0peXy2H7GvdsTLr3EvwE%2BkQIqnggSASFMO3TARrxwPH%2Br6qdRS0ngd0WQ%3D%3D&numOfRows=10&pageNo=1&base_date=20241028&base_time=0600&nx=61&ny=125 app listening on port 3000!"
+  );
 });
